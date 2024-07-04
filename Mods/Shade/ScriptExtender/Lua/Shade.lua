14906c993f21bd66e97f38163a5b2187861d2585
Lunisole <rverneret@laposte.net>
2024-07-04 13:02:52 +0200

    testing shadow

--- Mods/Shade/ScriptExtender/Lua/Shade.lua
+++ Mods/Shade/ScriptExtender/Lua/Shade.lua
@@ -1,6 +1,6 @@
 --[[
 --The sad shade of my former function :( :( :( :( :(
-function BackstabbingCheck(target, caster, distance)
+function Lu_Shde_BackstabbingCheck(target, caster, distance)
     local anglemin = ((3*math.pi)/4)
     local anglemax = ((5*math.pi)/4)
     local quatx = Ext.Entity.Get(target).Bound.Bound.RotationQuat[2]
@@ -20,8 +20,20 @@ function BackstabbingCheck(target, caster, distance)
 end
 ]]
 
+Ext.Vars.RegisterUserVariable("Lu_Shde_SpringHeeledAssStartPos", {
+    Server = true
+})
+
+Ext.Vars.RegisterUserVariable("Lu_Shde_DeathMark", {
+    Server = true
+})
+
+Ext.Vars.RegisterUserVariable("Lu_UmBld_ShdwPosition", {
+    Server = true
+})
+
 -- Determines the min/max angle required to backstab, depending on the spell.
-function BackstabbingAngle(spell)
+function Lu_Shde_BackstabbingAngle(spell)
     if (spell == "Shade_Feint_Stabbing_E00001" or spell == "Shade_Melee_MH_Feint_Stabbing_700002" or spell == "Shade_Melee_OH_Feint_Stabbing_700003" or spell == "Shade_Ranged_MH_Feint_Stabbing_800001" or spell == "Shade_Ranged_OH_Feint_Stabbing_800002") then 
         anglemin = 0
         anglemax = 2*math.pi
@@ -32,7 +44,7 @@ function BackstabbingAngle(spell)
     return anglemin, anglemax
 end 
 
-function charactersteeringvector(target)
+function Lu_Shde_CharSteeringVec(target)
     local targetzsteering = math.cos(Ext.Entity.Get(target).Steering.field_C)
     local targetxsteering = math.sin(Ext.Entity.Get(target).Steering.field_C)
     orient = {targetxsteering,0,targetzsteering}
@@ -40,9 +52,9 @@ function charactersteeringvector(target)
 end 
 
 -- The absolute core function of the class. It's this function wich determines if a character is in the back of another one or not.
-function BackstabbingCheck(target, caster, distance)
+function Lu_Shde_BackstabbingCheck(target, caster, distance)
     -- If the character doesn't have backstab extended passive then use those anglemin/max
-    charactersteeringvector(target)
+    Lu_Shde_CharSteeringVec(target)
     local normalized = Ext.Math.Normalize(distance)
     local DP = Ext.Math.Dot(orient,normalized)
     local result = Ext.Math.Acos(DP)
@@ -56,7 +68,7 @@ function BackstabbingCheck(target, caster, distance)
 end
 
 -- The second core function of the class. Iterates every entity in a combat with a character (shade) and calculates its distance to the character (shade).
-function BackstabbingApply(shade, spell, backstabbingmaxdistance)
+function Lu_Shde_BackstabbingApply(shade, spell, backstabbingmaxdistance)
     for k, v in pairs(Osi.DB_Is_InCombat:Get(nil, Osi.CombatGetGuidFor(Ext.Entity.Get(shade).Uuid.EntityUuid))) do
         local selfx = Ext.Entity.Get(shade).Bound.Bound.Translate[1]
         local selfz = Ext.Entity.Get(shade).Bound.Bound.Translate[3]
@@ -65,8 +77,8 @@ function BackstabbingApply(shade, spell, backstabbingmaxdistance)
         local distance = {targetx-selfx,0,targetz-selfz}
         if Ext.Math.Length(distance) <= backstabbingmaxdistance then
             Osi.RemoveStatus(v[1],"BACKSTABBING_TECHNICAL_200001")
-            BackstabbingAngle(spell)
-            BackstabbingCheck(v[1],shade, distance)
+            Lu_Shde_BackstabbingAngle(spell)
+            Lu_Shde_BackstabbingCheck(v[1],shade, distance)
             if isbackstabbing == 1 then
                 Osi.ApplyStatus(v[1],"BACKSTABBING_TECHNICAL_200001",5.0,1)
             end
@@ -74,9 +86,12 @@ function BackstabbingApply(shade, spell, backstabbingmaxdistance)
     end
 end
 
-function SpringHeeledAssassinApply(shade,posatturnstartx,posatturnstarty,posatturnstartz)
+function Lu_Shde_SpringHeeledAssApply(shade)
+    local posatturnstartx = Ext.Entity.Get(shade).Vars.Lu_Shde_SpringHeeledAssStartPos[1]
+    local posatturnstarty = Ext.Entity.Get(shade).Vars.Lu_Shde_SpringHeeledAssStartPos[2]
+    local posatturnstartz = Ext.Entity.Get(shade).Vars.Lu_Shde_SpringHeeledAssStartPos[3]
     for k, v in pairs(Osi.DB_Is_InCombat:Get(nil, Osi.CombatGetGuidFor(Ext.Entity.Get(shade).Uuid.EntityUuid))) do
-        targetpostx,targetposty,targetpostz = Osi.GetPosition(v[1])
+        local targetpostx,targetposty,targetpostz = Osi.GetPosition(v[1])
         local distance = Ext.Math.Length({targetpostx-posatturnstartx,targetposty-posatturnstarty,targetpostz-posatturnstartz})
         if (7 <= distance and distance <= 11) then
             Osi.ApplyStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_1_200029",5.0,1)
@@ -88,18 +103,56 @@ function SpringHeeledAssassinApply(shade,posatturnstartx,posatturnstarty,posattu
             Osi.ApplyStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_4_200032",5.0,1)
         elseif (29 < distance) then
             Osi.ApplyStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_5_200033",5.0,1)
+        else
+            Osi.RemoveStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_1_200029")
+            Osi.RemoveStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_2_200030")
+            Osi.RemoveStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_3_200031")
+            Osi.RemoveStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_4_200032")
+            Osi.RemoveStatus(v[1],"SRING_HEELED_ASSASSIN_TECHNICAL_5_200033")
         end
     end
 end
 
-function shadeteleportto()
+function Lu_Shde_TeleportTo()
     Osi.TeleportToPosition(shdtotp, positionrectified[1], positionrectified[2], positionrectified[3], "", 0, 0, 0)
+end
+
+function Lu_Shde_DeathMarkInit(marked4death, shade)
+    Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark = {0,Ext.Entity.Get(shade).Uuid.EntityUuid}
+end
+
+function Lu_Shde_DeathMarkInrmnt(marked4death, shade, DamageAmount)
+    if Ext.Entity.Get(shade).Uuid.EntityUuid == Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[2] then
+        if Osi.HasActiveStatus(marked4death,"BACKSTABBING_TECHNICAL_200001") == 1 then
+            Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[1] = (Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[1]+math.floor(3/2*DamageAmount))
+        else 
+            Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[1] = (Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[1]+DamageAmount)
+        end
+    end
+end
+
+function Lu_Shde_DeathMarkExpi(marked4death)
+    local Damage = math.floor(4/3*Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[1])
+    ApplyDamage(marked4death,Damage,"Piercing",Ext.Entity.Get(marked4death).Vars.Lu_Shde_DeathMark[2])
+end
+
+function Lu_UmBld_ShdwInit(shadow, shade)
+    Osi.Transform(shadow,shade,"585ee1db-2c04-4e6f-874a-2b45fd554052")
+    local x,y,z = Osi.GetPosition(shadow)
+    Ext.Entity.Get(shade).Vars.Lu_UmBld_ShdwPosition = {x,y,z,shadow}
+end
+
+function Lu_UmBld_ShdwSwap(shade)
+    shdtotp = shade
+    positionrectified = {Ext.Entity.Get(shade).Vars.Lu_UmBld_ShdwPosition[1],Ext.Entity.Get(shade).Vars.Lu_UmBld_ShdwPosition[2],Ext.Entity.Get(shade).Vars.Lu_UmBld_ShdwPosition[3]}
+    Osi.Die(Ext.Entity.Get(shade).Vars.Lu_UmBld_ShdwPosition[4])
+    Ext.Timer.WaitFor(1150, Lu_Shde_TeleportTo)
 end
 
 -- The main listening. This is the one which applies backstabbing.
 Ext.Osiris.RegisterListener("StartedPreviewingSpell", 4, "before", function (caster, spell, _, _, _)
     if HasPassive(caster,"Shade_Innate_Backstabbing_100001") == 1 then
-        BackstabbingApply(caster,spell,15)
+        Lu_Shde_BackstabbingApply(caster,spell,15)
     end
 end)
 
@@ -165,19 +218,22 @@ Ext.Osiris.RegisterListener("UsingSpellOnTarget", 6, "before", function (shade, 
     end
 end)
 
+--The listener to get the position on turn start of Spring-Heeled Assassin
 Ext.Osiris.RegisterListener("TurnStarted", 1, "before", function(shade)
     if (HasPassive(shade,"Shade_Spring_Heeled_Assassin_100021") == 1) then
-        posatstartx,posatstarty,posatstartz = Osi.GetPosition(shade)
-        _D(posatstartx,posatstarty,posatstartz)
+        local posatstartx,posatstarty,posatstartz = Osi.GetPosition(shade)
+        Ext.Entity.Get(shade).Vars.Lu_Shde_SpringHeeledAssStartPos = {posatstartx,posatstarty,posatstartz}
     end
 end)
 
+--The listener to apply the status of Spring-Heeled Assassin
 Ext.Osiris.RegisterListener("StartedPreviewingSpell", 4, "before", function(shade, _, _, _, _)
     if HasPassive(shade,"Shade_Spring_Heeled_Assassin_100021") == 1 then
-        SpringHeeledAssassinApply(shade,posatstartx,posatstarty,posatstartz)
+        Lu_Shde_SpringHeeledAssApply(shade)
     end
 end)
 
+--Maybe useless, I'm not sure
 Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(shade, status, _, _)
 	if status == "SMOKE_COVER_EFFECT_A00001" then
         Ext.Timer.WaitFor(1500, function(shade)
@@ -187,25 +243,47 @@ Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(shade, status,
 end)
 
 Ext.Osiris.RegisterListener("AttackedBy",7,"after", function(shade,attacker,_,_,_,_,_)
-    if HasPassive(shade,"Test_testLUL") then
-        charactersteeringvector(attacker)
+    if HasPassive(shade,"Shade_Gap_Close_F00004") then
+        Lu_Shde_CharSteeringVec(attacker)
         local posx,posy,posz = Osi.GetPosition(attacker)
         positionrectified = {posx+orient[1],posy+orient[2],posz+orient[3]}
     end
 end)
 
-Ext.Osiris.RegisterListener("UsingSpell", 5, "after", function (shade, spell, _, _, _)
-    if spell == "Shade_Gap_Close_700008" then
+Ext.Osiris.RegisterListener("UsingSpell", 5, "after", function (shade,spell,_,_,_)
+    if spell == "Shade_Gap_Close_C00004" then
         shdtotp = shade
-        Ext.Timer.WaitFor(1300, shadeteleportto)
+        Ext.Timer.WaitFor(1300, Lu_Shde_TeleportTo)
     end
 end)
 
-Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(marked4death, status, _, _)
-	if status == "" then
-        local startinghp = Ext.Entity.Get(marked4death).Health.Hp
-        local startingthp = Ext.Entity.Get(marked4death).Health.TemporaryHp
-        local startingvalue = startinghp + startingthp
-        _P(startinghp,startingthp,startingvalue)
+Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(marked4death,status,shade, _)
+    if status == "MARK_OF_DEATH_200042" then
+        Lu_Shde_DeathMarkInit(marked4death, shade)
     end
 end)
+
+Ext.Osiris.RegisterListener("AttackedBy",7,"after", function(marked4death,shade,_,_,DamageAmount,_,_)
+    if Osi.HasActiveStatus(marked4death,"MARK_OF_DEATH_200042") == 1 then
+        Lu_Shde_DeathMarkInrmnt(marked4death,shade,DamageAmount)
+    end
+end)
+
+Ext.Osiris.RegisterListener("StatusRemoved", 4, "after", function(marked4death,status,_,_)
+    if status == "MARK_OF_DEATH_200042" then
+        Lu_Shde_DeathMarkExpi(marked4death)
+    end
+end)
+
+Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(shadow,status,shade, _)
+    if status == "SHADOW_EFFECT_A00002" then
+        Lu_UmBld_ShdwInit(shadow,shade)
+    end
+end)
+
+Ext.Osiris.RegisterListener("UsingSpell", 5, "after", function (shade,spell,_,_,_)
+    if spell == "Shade_Swap_TEST" then
+        Lu_UmBld_ShdwSwap(shade)
+    end
+end)
+
